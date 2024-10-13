73612bd9c7e5c940e4a07ff16c3030d6b193906c
Dipakxettri <codeepakghimire@gmail.com>
2024-10-13 11:56:40 +0545

    update

---
+++ WebDevelopment/frontend-development/Javascript/Stage03_Opertors&Conditionals/01main.js
@@ -0,0 +1,25 @@
+//operators :- this is single line comment double slash is use to create a comments 
+/*Operators : 
+multi line comment
+*/
+
+
+/*
+1) Arithmetic => +,-,*,/
+
+2) Assignment => =,+=,-=,*=,/=,××=
+
+3) Comparison => ==,===,!=,!==
+    <,<=,>,>=
+    
+4) Logical => &&,||,!
+
+5) Tinary 
+=> syntax:
+condition? trueOutput : falseOutput
+*/
+//code:
+let age = 18;
+let result = age>18? " adult " : "not adult";
+document.write(result)//prints in HTML page
+
---
+++ WebDevelopment/frontend-development/Javascript/Stage03_Opertors&Conditionals/02main.js
@@ -0,0 +1,22 @@
+//Conditional Statements
+
+/*
+if
+else if
+else
+*/
+
+//code:
+let mode = "dark-mode";
+let color;
+if(mode === "dark-mode"){
+  color = "black";
+}
+else if(mode == "light-mode"){
+  color = "white";
+}
+else{
+  color = "white";
+}
+
+document.write(color);
