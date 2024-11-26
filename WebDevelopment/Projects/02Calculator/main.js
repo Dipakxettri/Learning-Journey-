02b48b70abbb014f065f67c1a047a920cc1d1625
Deepak <codeepakghimire@gmail.com>
2024-11-25 18:29:15 -0800

    updated

--- WebDevelopment/Projects/02Calculator/main.js
+++ WebDevelopment/Projects/02Calculator/main.js
@@ -4,22 +4,29 @@ const bg = document.querySelector(".container");
 const tB = document.getElementById("toggleB");
 
 const display = document.getElementById("displayF");
+const calc = document.querySelector(".calc");
+const buttons = document.querySelector(".button");
 
 
 tB.addEventListener("click",()=>{
     
     
     if(tB.textContent == "Dark Mode"){
-        bg.style.backgroundColor ="black";
+        // bg.style.backgroundColor ="black";
         tB.style.backgroundColor = "white";
         tB.textContent = "White Mode";
         tB.style.color = "black";
+        calc.style.backgroundColor = "#090004";
     }
     else{
         bg.style.backgroundColor = "rgb(213, 206, 206)";
         tB.textContent = "Dark Mode";
         tB.style.backgroundColor = "black";
         tB.style.color = "white";
+        calc.style.backgroundColor = "white";
+
+
+
        
 
     }
